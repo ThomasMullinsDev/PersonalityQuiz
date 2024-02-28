d1fb6b3602bb40704023d07cbb81c6befddd2517
Thomas Mullins <thomasmullinsdev@gmail.com>
2024-02-28 10:26:32 -0700

    Worked on the project still requires some more work

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Thomas Mullins on 2/27/24.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PersonalityQuiz/PersonalityQuiz/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  PersonalityQuiz
+//
+//  Created by Thomas Mullins on 2/28/24.
+//
+
+import Foundation
